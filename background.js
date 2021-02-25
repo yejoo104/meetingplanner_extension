ea9a412c1d6df715f2706c75b8d6482f33ba957d
한예주 <yejoo_han@college.harvard.edu>
2021-02-26 01:47:23 +0900

    background script responds to when user is on when2meet

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log('from background')
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        if (/^https:\/\/www\.when2meet\.com/.test(current_tab_info.url)) {
+            console.log("background only if on right site")
+        }
+    })
+})
