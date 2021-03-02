fc8ac281667f02900739ef1cc1ff3707f2c60079
한예주 <yejoo_han@college.harvard.edu>
2021-03-02 22:27:28 +0900

    added foreground script

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
         if (/^https:\/\/www\.when2meet\.com/.test(current_tab_info.url)) {
-            console.log("background only if on right site")
+            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected'))
         }
     })
 })
