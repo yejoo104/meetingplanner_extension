65f2f5291da38a575eb1b8a2c2ac4e47c5c89a33
한예주 <yejoo_han@college.harvard.edu>
2021-03-04 02:21:17 +0900

    adds record button when onUpdated

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
         if (/^https:\/\/www\.when2meet\.com/.test(current_tab_info.url)) {
-            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected'))
+            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected1'))
         }
     })
-})
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.get(tabId, current_tab_info => {
+        if (/^https:\/\/www\.when2meet\.com/.test(current_tab_info.url)) {
+            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected2'))
+        }
+    })
+});
