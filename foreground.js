57b5e89c71ac3fee388b8fcc2c02248bcc3489c5
한예주 <yejoo_han@college.harvard.edu>
2021-03-05 01:57:05 +0900

    foreground.js can iterate through when2meet grids and print them

--- foreground.js
+++ foreground.js
@@ -5,3 +5,22 @@
 
     document.querySelector('#GroupAvailability').appendChild(record);
 }
+
+record.addEventListener('click', () => {
+    var groupgrid = document.querySelector('#GroupGridSlots.GroupGrid');
+    walkTheDOM(groupgrid, print);
+})
+
+function walkTheDOM(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node)
+    {
+        walkTheDOM(node, func);
+        node = node.nextSibling;
+    }
+};
+
+function print(node) {
+    console.log(node);
+};
