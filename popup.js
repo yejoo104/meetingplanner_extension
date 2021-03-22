d1ba9a85fcd73a701e14a8078404e8b48774cc6c
한예주 <yejoo_han@college.harvard.edu>
2021-03-22 15:12:57 +0900

    added validation after form submission

--- popup.js
+++ popup.js
@@ -1,6 +1,22 @@
 
-var button = document.getElementById("button-schedule");
+var button = document.getElementById("schedule-form");
 
-button.addEventListener('click', () => {
+button.addEventListener('submit', () => {
+    var min_event = document.getElementById("min_event").value;
+    var max_event = document.getElementById("max_event").value;
+    
+    if (min_event && max_event && min_event > max_event) {
+        alert("Minimum number of events cannot be bigger than the maximum number of events");
+        return;
+    }
+    
+    var min_people = document.getElementById("min_people").value;
+    var max_people = document.getElementById("max_people").value;
+    
+    if (min_people && max_people && min_people > max_people){
+        alert("Minimum number of people cannot be bigger than the maximum number of people");
+        return;
+    }
+    
     chrome.runtime.sendMessage({message: 'schedule'});
 })
