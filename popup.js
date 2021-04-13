f6bf69a45ef1f82971de837a6549a22d77aa00c6
한예주 <yejoo_han@college.harvard.edu>
2021-04-13 21:10:11 +0900

    implemented next_slot function in popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,20 @@
 
+function next_slot(slot, interval){
+    var year = parseInt(slot.slice(0, 4));
+    var month = parseInt(slot.slice(4, 6));
+    var day = parseInt(slot.slice(6, 8));
+    var time = slot.slice(8);
+    var hour = parseInt(time.slice(0, -2));
+    var minutes = parseInt(time.slice(-2));
+    
+    var slot_date = new Date(year, month - 1, day, hour , minutes);
+    var new_date = new Date(slot_date.getTime() + interval * 60000);
+    
+    var date_string = new_date.getFullYear().toString() + ("0" + (new_date.getMonth() + 1).toString()).slice(-2) + ("0" + new_date.getDate().toString()).slice(-2) + ("0" + new_date.getHours().toString()).slice(-2) + ("0" + new_date.getMinutes().toString()).slice(-2);
+    
+    return date_string;
+}
+
 var button = document.getElementById("schedule-form");
 
 button.addEventListener('submit', () => {
@@ -20,6 +36,6 @@ button.addEventListener('submit', () => {
     }
     
     chrome.storage.local.get(['slots', 'slot_dict','dates', 'start_time', 'end_time'], function(result) {
-        alert(result['slots']);
+        alert(next_slot(result['slots'][10], 15));
     })
 })
